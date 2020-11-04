3ae2bb4c7a15cab4d13172a26915eb21333181e3
Fernando Barbosa <fernando.vitor@g.globo>
2020-11-04 09:25:43 -0300

    ansible

---
+++ ubuntu_bionic64_ansible/src/index.php
@@ -0,0 +1,16 @@
+<?php
+  echo "Testando conexao <br /> <br />";
+  $servername = "192.168.0.61";
+  $username = "phpuser";
+  $password = "pass";
+
+  // create connection
+  $conn = new mysqli($servername, $username, $password);
+
+  // check connection
+  if ($conn->connect_error) {
+    die("Conexão falhou: " . $conn->$connect_error);
+  }
+
+  echo "Conectado com sucesso";
+?>
